4b7be205efb4f59693952d47ddd95f3f286b2cbd
AlgoWebBot <shimulzahan636@gmail.com>
2024-04-29 00:07:16 +0600

    bla bla bla

---
+++ src/Components/SpooterRegistra/SoopReg.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SoopReg = () => {
+  return (
+    <div>SoopReg</div>
+  )
+}
+
+export default SoopReg
---
+++ src/Components/Steper/Steper.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Steper = () => {
+    return (
+        <div>Steper</div>
+    )
+}
+
+export default Steper
