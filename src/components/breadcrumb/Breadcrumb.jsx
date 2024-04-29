217f033a0ae9b26884176e1aa41cc4e15e78f406
md-ali-0 <mohammad.98482@gmail.com>
2024-04-29 12:58:50 +0600

    Breadcrumb Updated

--- src/components/breadcrumb/Breadcrumb.jsx
+++ src/components/breadcrumb/Breadcrumb.jsx
@@ -1,3 +1,4 @@
+import { IoIosArrowForward } from "react-icons/io";
 import { Link } from "react-router-dom";
 import BreadCrumbImage from "../../assets/images/banner.svg";
 
@@ -10,7 +11,7 @@ const Breadcrumb = ({ title }) => {
             <div className="flex items-center max-w-6xl mx-auto h-72 px-6 lg:px-0">
                 <div className="space-y-3">
                     <h3 className="text-white text-3xl font-bold">{title}</h3>
-                    <h3 className="text-white text-lg font-semibold"> <Link to={'/'}>Home</Link> {">"} {title}</h3>
+                    <h3 className="text-white font-medium"> <Link to={'/'}>Home</Link> <IoIosArrowForward className="inline" size={15}/> {title}</h3>
                 </div>
             </div>
         </div>
