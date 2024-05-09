3e31eb189d42a837fa0a19a2f401208d7478be1e
md-ali-0 <mohammad.98482@gmail.com>
2024-05-09 21:04:10 +0600

    home page search updated

--- src/Pages/Home/ReducedPrice.jsx
+++ src/Pages/Home/ReducedPrice.jsx
@@ -3,7 +3,7 @@ import Container from "../../components/Container/Container";
 import PropertyCard from "../../components/cards/PropertyCard/PropertyCard";
 import SectionTitle from "../../components/sectionTitle/SectionTitle";
 
-const ReducedPrice = ({ mainData, filterData}) => {
+const ReducedPrice = ({ mainData, filterData }) => {
     // console.log(listings);
     return (
         <section className="bg-white py-10 px-6 md:px-0" id="reduceprice">
@@ -11,21 +11,26 @@ const ReducedPrice = ({ mainData, filterData}) => {
                 heading={"Reduced Price"}
                 subheading={"Exploring Reduced Property Lists"}
             />
-            <Container>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-10">
-                    {mainData.filter(filterData).slice(0, 6).map((item) => (
-                        <PropertyCard key={item._id} item={item} />
-                    ))}
-                </div>
-
-                <Link to="/buy">
-                    <div className="flex justify-center mt-10">
-                        <button className="btn-primary text-white py-2 px-5 rounded mt-4">
-                            See All
-                        </button>
+            {mainData && (
+                <Container>
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-10">
+                        {mainData
+                            ?.filter(filterData)
+                            .slice(0, 6)
+                            .map((item) => (
+                                <PropertyCard key={item._id} item={item} />
+                            ))}
                     </div>
-                </Link>
-            </Container>
+
+                    <Link to="/buy">
+                        <div className="flex justify-center mt-10">
+                            <button className="btn-primary text-white py-2 px-5 rounded mt-4">
+                                See All
+                            </button>
+                        </div>
+                    </Link>
+                </Container>
+            )}
         </section>
     );
 };
